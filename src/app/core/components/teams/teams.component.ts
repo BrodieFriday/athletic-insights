6e7cecebeeaf4301f4dfaf8e9031901c5db28371
Brodie Friday <brodieleefriday@gmail.com>
2024-12-31 15:25:04 -0600

    Get teams component working

--- src/app/core/components/teams/teams.component.ts
+++ src/app/core/components/teams/teams.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { StatsService } from '../../services/stats.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Team } from '../../model/team';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-teams',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule],
   templateUrl: './teams.component.html',
-  styleUrl: './teams.component.scss'
+  styleUrl: './teams.component.scss',
 })
 export class TeamsComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  public dataSource = new MatTableDataSource<Team>();
+  public displayedColumns: string[] = [
+    'name',
+    'conference',
+    'division',
+    'location',
+  ];
 
-  constructor(private statsService: StatsService) {
-
-  }
+  constructor(private statsService: StatsService) {}
 
   ngOnInit() {
-    this.statsService.getData().subscribe(data => {
-      console.log("data",data);
+    this.statsService.getData().subscribe((data) => {
+      this.dataSource.data = data.data;
+      this.dataSource.paginator = this.paginator;
     });
   }
 
