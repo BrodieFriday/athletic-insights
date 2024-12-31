5f47025540ec5d0c07ce0ca65e1f1c6e74c30474
Brodie Friday <brodieleefriday@gmail.com>
2024-12-31 15:52:24 -0600

    Add sort headers

--- src/app/core/components/games/games.component.ts
+++ src/app/core/components/games/games.component.ts
@@ -4,16 +4,18 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { Game } from '../../model/game';
 import { CommonModule } from '@angular/common';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, CommonModule],
+  imports: [MatTableModule, MatPaginatorModule, CommonModule, MatSortModule],
   templateUrl: './games.component.html',
   styleUrl: './games.component.scss',
 })
 export class GamesComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   public dataSource = new MatTableDataSource<Game>();
   public displayedColumns: string[] = [
     'home_team',
@@ -29,6 +31,7 @@ export class GamesComponent implements OnInit {
     this.statsService.getGames().subscribe((data) => {
       this.dataSource.data = data.data;
       this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
   }
 }
